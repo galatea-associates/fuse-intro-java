ba613295d337b71676d0f5bd88280b3ddaadb2cf
Kevin Ramos <Kevramos93@yahoo.com>
2022-11-08 15:11:25 -0500

    undo revert

--- src/main/java/org/galatea/starter/service/IexService.java
+++ src/main/java/org/galatea/starter/service/IexService.java
@@ -8,9 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.galatea.starter.domain.IexHistoricalPrices;
 import org.galatea.starter.domain.IexLastTradedPrice;
 import org.galatea.starter.domain.IexSymbol;
+import org.galatea.starter.entrypoint.exception.EntityNotFoundException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
-
+import org.apache.commons.lang3.StringUtils;
 /**
  * A layer for transformation, aggregation, and business required when retrieving data from IEX.
  */
@@ -51,18 +53,26 @@ public class IexService {
    * @param range an optional range to check
    * @param date an optional date to check
    */
-  public List<IexHistoricalPrices> getHistoricalPricesForSymbols(final String symbol, final String range, final String date) {
+  public List<IexHistoricalPrices> getHistoricalPricesForSymbols(final String symbol, final String range, final String date) throws Exception{
 
-    if (symbol == null) { // No symbol
-      return iexClient.getHistoricalPrices();
-    } else if(range == null && date == null){ // Only symbol specified
+    // Input validation
+    if(StringUtils.isBlank(symbol)){ // Symbol is required
+      throw new EntityNotFoundException(symbol.getClass(), symbol);
+    } else if(StringUtils.isBlank(range) && StringUtils.isBlank(date)){ // No range or date specified
       return iexClient.getHistoricalPricesForSymbol(symbol);
-    } else if (range != null && date == null) { // Range specified but not date
+    } else if ((StringUtils.isBlank(range) == false) && StringUtils.isBlank(date)) { // Range specified but not date
+      if(range == ""){
+        throw new HttpMessageNotReadableException("\"range\" is not allowed to be empty");
+      }
       return iexClient.getHistoricalPricesRange(symbol, range);
-    } else if (range == null && date != null) { // Date specified but not range
+    } else if ((StringUtils.isBlank(range)) && StringUtils.isBlank(date) == false) { // Date specified but not range
+      if(date == ""){
+        throw new HttpMessageNotReadableException("\"date\" is not allowed to be empty");
+      }
       return iexClient.getHistoricalPricesDate(symbol, date);
     }
 
+    // All parameters were valid
     return iexClient.getHistoricalPricesForSymbols(symbol, range, date);
   }
 }
